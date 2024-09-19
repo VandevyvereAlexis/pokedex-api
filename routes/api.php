5fa62b2da8411778f3041bf7d6032da9ac9f7412
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-09-19 16:04:02 +0200

    Configuration dynamique des routes API dans api.php en utilisant un tableau et une boucle foreach pour générer les ressources automatiquement.

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+/*
+|--------------------------------------------------------------------------|
+|   Configuration Dynamique des Routes API                                 |
+|--------------------------------------------------------------------------|
+*/
+$resources = [
+    'creatures' => App\Http\Controllers\API\CreatureController::class,
+    'races' => App\Http\Controllers\API\RaceController::class,
+    'types' => App\Http\Controllers\API\TypeController::class,
+    'users' => App\Http\Controllers\API\UserController::class,
+];
+
+foreach ($resources as $resource => $controller) {
+    Route::apiResource($resource, $controller);
+}
+
+
+/*
+|--------------------------------------------------------------------------|
+|   Définition Manuelle des Routes API                                     |
+|--------------------------------------------------------------------------|
+*/
+// Route::apiResource("creatures", App\Http\Controllers\API\CreatureController::class);
+// Route::apiResource("races", App\Http\Controllers\API\RaceController::class);
+// Route::apiResource("types", App\Http\Controllers\API\TypeController::class);
+// Route::apiResource("users", App\Http\Controllers\API\UserController::class);
