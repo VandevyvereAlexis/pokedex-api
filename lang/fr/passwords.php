caa282423419b73aa737566c7566b1e750c8ade3
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-09-26 22:24:37 +0200

    Publication et configuration des fichiers de langue en français pour l'authentification

---
+++ lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langue de réinitialisation de mot de passe
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont les lignes par défaut qui correspondent
+    | aux raisons données par le courtier de mots de passe pour le résultat
+    | d'une tentative de mise à jour de mot de passe, comme un échec en raison
+    | d'un mot de passe invalide ou d'un jeton de réinitialisation.
+    |
+    */
+
+    'reset'     => 'Votre mot de passe a été réinitialisé.',
+    'sent'      => 'Nous avons envoyé le lien de réinitialisation de votre mot de passe par email.',
+    'throttled' => 'Veuillez attendre avant de réessayer.',
+    'token'     => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user'      => "Nous ne pouvons pas trouver d'utilisateur avec cette adresse email.",
+
+];
